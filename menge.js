8e646717d232f4a2aa342a61f017787dc104c702
kawanet <u-suke@kawa.net>
2015-06-24 22:03:10 +0900

    unique()

--- menge.js
+++ menge.js
@@ -13,10 +13,45 @@
     has: has,
     hasno: hasno,
     union: union,
-    except: except
+    except: except,
+    unique: unique
   };
 
   var array = [];
+
+  /**
+   * unique
+   *
+   * @param source {Array-like}
+   * @param [dest] {Array-like}
+   * @returns {Array-like}
+   */
+
+  function unique(source, dest) {
+    if (dest) {
+      array.forEach.call(source, function(value) {
+        if (!has(dest, value)) array.push.call(dest, value);
+      });
+      return dest;
+    } else {
+      array.reduceRight.call(source, function(prev, value, index) {
+        var hit = (index > 0) && hasWithLimit(source, value, index - 1);
+        if (hit) array.splice.call(source, index, 1);
+      }, null);
+      return source;
+    }
+  }
+
+  function hasWithLimit(source, value, limit) {
+    var found = false;
+    array.some.call(source, eqWithLimit);
+    return found;
+
+    function eqWithLimit(test, index) {
+      found = (test === value);
+      return found || (index >= limit);
+    }
+  }
 
   /**
    * except
@@ -94,6 +129,7 @@
    */
 
   function hasno(source, value) {
+    // return !has(source, value);
     return array.every.call(source, ne);
 
     function ne(test) {
---
+++ test/14.unique.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env ./node_modules/.bin/mocha -R spec
+
+var assert = require("assert");
+
+var OBJECT = {"object": 1};
+var ARRAY = ["array"];
+var NUMBER = 0;
+var NULL = null;
+var UNDEFINED = void 0;
+
+var LIST1 = ["hoge", "pomu", "hoge", "hoge", "fuga", "fuga"];
+var LIST2 = ["hoge", "pomu", "fuga"];
+
+var testjs = __filename.replace(/^.*\//, "");
+var mengejs = process.env.MENGEJS || "../menge.js";
+var short = mengejs.replace(/^.*\//, "");
+
+describe(testjs + " with " + short + " tests", function() {
+  var menge = require(mengejs);
+
+  it("unique(source)", function() {
+    var source = new ArrayLike(LIST1);
+    var result = menge.unique(source);
+
+    assert.equal(source, result);
+    assert.equal(join(LIST2), join(result));
+    assert.equal(3, source.length); // breaking
+    assert.equal(3, result.length);
+  });
+
+  it("unique(source, dest)", function() {
+    var source = new ArrayLike(LIST1);
+    var dest = new ArrayLike();
+    var result = menge.unique(source, dest);
+
+    assert.equal(dest, result);
+    assert.equal(join(LIST2), join(result));
+    assert.equal(6, source.length); // not breaking
+    assert.equal(3, dest.length);
+    assert.equal(3, result.length);
+  });
+});
+
+function ArrayLike(array) {
+  if (!(this instanceof ArrayLike)) return new ArrayLike(array);
+  Array.prototype.push.apply(this, array);
+}
+
+function join(array) {
+  return Array.prototype.join.call(array, ",");
+}
